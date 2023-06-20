47dd8055daafcfacb965fd7f5071516d08974723
npokrandt <npokrandt919@gmail.com>
2023-06-19 23:21:27 -0500

    added moer css and sorta started creating coffee card elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 
-var btn = document.querySelector("button")
+var startOrdersBtn = document.getElementById("start-orders")
 
+// 2 stages:
+// build order card
+// check the users answer to the real one
 // main code
 function startGame(){
     document.body.style.backgroundColor = "lightgreen"
+    var parent = document.getElementById("coffee-area")
+    console.log(parent)
+    var coffeeCard = document.createElement("form")
+    coffeeCard.className = "coffee-card"
+    //coffeeCard.style.backgroundColor = "green"
+    console.log(coffeeCard)
+    parent.appendChild(coffeeCard)
+
+    parent = document.querySelector("form")
+    console.log(parent)
+    var coffeeHeader = document.createElement("h4")
+    coffeeHeader.innerHTML = "Order Card"
+    console.log(coffeeHeader)
+    parent.appendChild(coffeeHeader)
 }
 
-btn.addEventListener("click", function() {
+startOrdersBtn.addEventListener("click", function() {
     startGame()
 })
